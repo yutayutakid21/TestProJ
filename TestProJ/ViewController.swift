227f14f49c4c0392aa2fd50faa6c2cf5d9a35c22
Yuta Fujii <yutayutakid21@gmail.com>
2019-12-19 19:12:34 +0900

    課題

--- TestProJ/ViewController.swift
+++ TestProJ/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /*
+     
+     課題
+     
+     NextViewController.swiftと画面を作り、受け取り用変数
+     
+     var text = String()
+     var userName = String()
+     var image = UIImage()
+     
+     を宣言し、
+     
+     画面にラベル2つ、UIImageView1つをつなぎ
+     
+     viewdidloadで表示するというコードを書いてpullRequestを送ってください。
+     
+     */
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
