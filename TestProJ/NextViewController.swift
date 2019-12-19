2bf17f21e908bb99222835c396f99f3b610b3a38
Fujio Nakatsuka <com.nifty.cbm01799>
2019-12-19 19:37:10 +0900

    Label2,UIImageviewを追加しました。

---
+++ TestProJ/NextViewController.swift
@@ -0,0 +1,43 @@
+//
+//  NextViewController.swift
+//  TestProJ
+//
+//  Created by 中塚富士雄 on 2019/12/19.
+//  Copyright © 2019 Fujii Yuta. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+    var text = String()
+    var userName = String()
+    var image = UIImage()
+    
+
+    override func viewDidLoad() {
+        
+        
+        
+        
+        
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var textLabel1: UILabel!
+    @IBOutlet weak var userNameLabel1: UILabel!
+    
+    @IBOutlet weak var UIImageview: UIImageView!
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
